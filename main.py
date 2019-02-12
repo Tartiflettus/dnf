59f00e75096714c2ab21e7e85d9cf73a710b0dd2
Victor <victor.creusel4@etu.univ-lorraine.fr>
2019-02-12 18:41:16 +0100

    First commit : non functionnal dnf

---
+++ main.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from random import uniform
+
+size = 25
+
+
+def init_array():
+    # return [[0. for j in range(size)] for i in range(size)]
+    return [[uniform(0, 1) for i in range(size)] for j in range(size)]
+
+
+def normalize(array):
+    min_array = min(map(min, array))
+    max_array = max(map(max, array))
+
+    for y in range(size):
+        for x in range(size):
+            array[y][x] = (array[y][x] - min_array) / (max_array - min_array)
+
+    """for y in range(size):
+        for x in range(size):
+            if array[y][x] < 0 or array[y][x] > 1:
+                print("normalize ERROR")"""
+
+
+# normalized euclidean distance
+def euclidean_dist(x, y):
+    (x1, y1) = x
+    (x2, y2) = y
+    x1 /= size
+    x2 /= size
+    y1 /= size
+    y2 /= size
+    ans = math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
+    #if ans > 2:
+    #    print("euclidean_dist ERROR")
+    return ans
+
+
+# variables globales
+cexc = 1.25
+sigmaexc = 0.1
+cinh = 0.7
+sigmainh = 10.
+array = init_array()
+dt = 0.1
+t = 0
+tau = 0.64
+
+
+def difference_of_gaussian(distance):
+    return cexc*math.exp(-distance*distance/(2.*sigmaexc*sigmaexc))
+    - cinh*math.exp(-distance*distance / (2.*sigmainh*sigmainh))
+
+
+def gaussian_activity(a, b, sigma):
+    activity = [[0 for j in range(size)] for i in range(size)]
+    (x1, y1) = a
+    (x2, y2) = b
+    x1 /= size
+    x2 /= size
+    y1 /= size
+    y2 /= size
+    # first gaussian
+    for y in range(size):
+        yn = y / size
+        for x in range(size):
+            xn = x / size
+            first_term = (xn-x1)*(xn-x1) / (2.*sigma*sigma)
+            second_term = (yn-y1)*(yn-y1) / (2.*sigma*sigma)
+            activity[y][x] = math.exp(-(first_term + second_term))
+
+    # second gaussian
+    for y in range(size):
+        yn = y / size
+        for x in range(size):
+            xn = x / size
+            first_term = (xn-x2)*(xn-x2) / (2.*sigma*sigma)
+            second_term = (yn-y2)*(yn-y2) / (2.*sigma*sigma)
+            activity[y][x] += math.exp(-(first_term + second_term))
+
+    normalize(activity)
+    return activity
+
+
+entry = gaussian_activity((10., 10.), (25., 25.), 2.)
+
+
+def update_neuron(x):
+    global array
+    global entry
+    (x, y) = x
+    # excitation term
+    exc_term = 0.
+    for yi in range(size):
+        for xi in range(size):
+            if xi != x or yi != y:
+                exc_term += array[yi][xi]*difference_of_gaussian( euclidean_dist((x, y), (xi, yi)) )
+    array[y][x] += dt*(-array[y][x] + exc_term + entry[y][x]) / tau
+
+    # avoid outliers
+    """if array[y][x] > 1:
+        array[y][x] = 1.
+    elif array[y][x] < 0:
+        array[y][x] = 0."""
+
+
+def synchronous_run():
+    global array
+    for y in range(size):
+        for x in range(size):
+            update_neuron((x, y))
+    normalize(array)
+            
+
+if __name__ == "__main__":
+    array = init_array()
+    entry = gaussian_activity((5, 5), (20, 20), 0.08)
+    diff = [[difference_of_gaussian(euclidean_dist((12, 12), (x, y)))
+             for x in range(size)] for y in range(size)]
+    fig = plt.figure()
+    plt.subplot(1, 3, 1)
+    im = plt.imshow(array, cmap='hot', interpolation='nearest', animated=True)
+    plt.subplot(1, 3, 2)
+    im_entry = plt.imshow(entry, cmap='hot', interpolation='nearest', animated=True)
+    plt.subplot(1, 3, 3)
+    im_diff = plt.imshow(diff, cmap='hot', interpolation='nearest', animated=True)
+
+    index = 0
+
+    def updatefig(*args):
+        global array
+        global entry
+        global index
+        synchronous_run()
+        im.set_array(array)
+        im_entry.set_array(entry)
+        im_diff.set_array(diff)
+        print(index)
+        index += 1
+        return im, im_entry, im_diff
+    
+    ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True)
+    plt.show()
